4c695312d68611fe6b60ef9e14096dc0ee8171f1
Maheshwar Tenneri <99230004+maheshwartenneri@users.noreply.github.com>
2024-08-15 18:35:45 -0400

    Update list_policies.py

--- list_policies.py
+++ list_policies.py
@@ -6,3 +6,21 @@ When comparing the Netty NIO Async HTTP Client and the AWS CRT Async Client, the
 	4.	Usability and Integration: Netty is more mature and widely adopted across various use cases, providing extensive documentation and community support. However, the AWS CRT client is specifically optimized for AWS SDKs and might offer more out-of-the-box benefits if your primary use case is AWS-related  .
 
 In summary, while Netty is a robust and flexible choice, the AWS CRT client offers superior performance and optimized features for AWS services, especially in resource-constrained environments like AWS Lambda. The choice between the two should be guided by your specific use case and performance requirements.
+
+Here are the sources separated under each point:
+
+### 1. Performance
+- [AWS CRT Async Client Performance](https://aws.amazon.com/blogs/developer/announcing-availability-of-the-aws-crt-http-client-in-the-aws-sdk-for-java-2-x/) [oai_citation:8,Announcing the general availability of the AWS CRT HTTP Client in the AWS SDK for Java 2.x](https://aws.amazon.com/about-aws/whats-new/2023/02/aws-crt-http-client-sdk-java-2-x/)
+- [Lambda Cold Start Time and Memory Consumption Comparison](https://dev.to/aws-builders/aws-sdk-for-java-2-x-asynchronous-http-clients-and-their-impact-on-cold-start-times-and-memory-consumption-of-lambda-and-java-11-5flm) [oai_citation:7,AWS SDK for Java 2.x asynchronous HTTP clients and their impact on cold start times and memory consumption of Lambda and Java 11 - DEV Community](https://dev.to/aws-builders/aws-sdk-for-java-2x-asynchronous-http-clients-and-their-impact-on-cold-start-times-and-memory-consumption-of-aws-lambda-366p)
+
+### 2. Connection Management
+- [AWS CRT Client Connection Management](https://aws.amazon.com/blogs/developer/announcing-availability-of-the-aws-crt-http-client-in-the-aws-sdk-for-java-2-x/) [oai_citation:6,Announcing the general availability of the AWS CRT HTTP Client in the AWS SDK for Java 2.x](https://aws.amazon.com/about-aws/whats-new/2023/02/aws-crt-http-client-sdk-java-2-x/)
+- [Detailed Features of the AWS CRT HTTP Client](https://aws.amazon.com/blogs/developer/introducing-aws-common-runtime-http-client-in-the-aws-sdk-for-java-2-x/) [oai_citation:5,Introducing AWS Common Runtime HTTP Client in the AWS SDK for Java 2.x | AWS Developer Tools Blog](https://aws.amazon.com/blogs/developer/introducing-aws-common-runtime-http-client-in-the-aws-sdk-for-java-2-x/)
+
+### 3. Memory Footprint
+- [AWS CRT Client Memory Usage](https://www.infoq.com/news/2023/02/aws-crt-http-client-java-sdk/) [oai_citation:4,Faster Startup Time and Lower Memory Usage: New CRT HTTP Client in AWS SDK for Java - InfoQ](https://www.infoq.com/news/2023/02/aws-sdk-java-crt-client/)
+- [Memory Impact Comparison in AWS Lambda](https://dev.to/aws-builders/aws-sdk-for-java-2-x-asynchronous-http-clients-and-their-impact-on-cold-start-times-and-memory-consumption-of-lambda-and-java-11-5flm) [oai_citation:3,AWS SDK for Java 2.x asynchronous HTTP clients and their impact on cold start times and memory consumption of Lambda and Java 11 - DEV Community](https://dev.to/aws-builders/aws-sdk-for-java-2x-asynchronous-http-clients-and-their-impact-on-cold-start-times-and-memory-consumption-of-aws-lambda-366p)
+
+### 4. Usability and Integration
+- [Netty Client Overview and Integration](https://aws.amazon.com/blogs/developer/announcing-availability-of-the-aws-crt-http-client-in-the-aws-sdk-for-java-2-x/) [oai_citation:2,Announcing the general availability of the AWS CRT HTTP Client in the AWS SDK for Java 2.x](https://aws.amazon.com/about-aws/whats-new/2023/02/aws-crt-http-client-sdk-java-2-x/)
+- [AWS CRT Integration Options](https://aws.amazon.com/blogs/developer/introducing-aws-common-runtime-http-client-in-the-aws-sdk-for-java-2-x/) [oai_citation:1,Introducing AWS Common Runtime HTTP Client in the AWS SDK for Java 2.x | AWS Developer Tools Blog](https://aws.amazon.com/blogs/developer/introducing-aws-common-runtime-http-client-in-the-aws-sdk-for-java-2-x/)
